f2500afdc4513f6ef21330f08a2eb7f4bca71f08
BasitSameerpro <summerkhan054@gmail.com>
2025-01-29 13:33:34 +0500

    worrrrrrk

--- backend/server.js
+++ backend/server.js
@@ -1,11 +1,11 @@
+// api/index.js
 const express = require("express");
 const cors = require("cors");
 require("dotenv").config();
-const connectDB = require("./config/db");
-const userRoutes = require("./routes/userRoutes");
+const connectDB = require("../config/db");
+const userRoutes = require("../routes/userRoutes");
 const cookieParser = require("cookie-parser");
-const { notFound, errorHandler } = require("./middleware/errorHandler");
-connectDB();
+const { notFound, errorHandler } = require("../middleware/errorHandler");
 
 const app = express();
 
@@ -20,11 +20,14 @@ app.use(
 app.use(express.json());
 app.use(express.urlencoded({ extended: true }));
 app.use(cookieParser());
+
+// Connect to database
+connectDB();
+
 app.use("/api/user/", userRoutes);
 
 app.use(notFound);
 app.use(errorHandler);
 
-app.listen(process.env.PORT, () => {
-  console.log(`Server running on port ${process.env.PORT}`);
-});
+// Export the Express API
+module.exports = app;

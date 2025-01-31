26138cd4b9c3c78f190a23044d6efdadbb1995d0
BasitSameerpro <summerkhan054@gmail.com>
2025-01-31 17:47:03 +0500

    .

--- backend/server.js
+++ backend/server.js
@@ -1,19 +1,18 @@
-const express = require("express");
-const cors = require("cors");
-require("dotenv").config();
-const connectDB = require("../config/db");
-const userRoutes = require("../routes/userRoutes");
-const cookieParser = require("cookie-parser");
-const { notFound, errorHandler } = require("../middleware/errorHandler");
+const express = require('express');
+const cors = require('cors');
+require('dotenv').config();
+const connectDB = require('../config/db');
+const userRoutes = require('../routes/userRoutes');
+const cookieParser = require('cookie-parser');
+const { notFound, errorHandler } = require('../middleware/errorHandler');
 
 const app = express();
 
-// Enable CORS for the frontend (you may need to adjust `BASE_URL` for production)
 app.use(
   cors({
     origin: process.env.BASE_URL,
     credentials: true,
-    methods: "PUT,POST,GET,DELETE,PATCH,HEAD",
+    methods: 'PUT,POST,GET,DELETE,PATCH,HEAD',
   })
 );
 
@@ -24,14 +23,10 @@ app.use(cookieParser());
 // Connect to database
 connectDB();
 
-// API routes
-app.use("/api/user/", userRoutes);
+app.use('/api/user', userRoutes);
 
-// Error handling middleware
+// Error Handling
 app.use(notFound);
 app.use(errorHandler);
 
-// Export the Express API for serverless function
-module.exports = (req, res) => {
-  app(req, res);
-};
+module.exports = app;
